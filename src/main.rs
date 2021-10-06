98b35672eb4ea02ee3f846873912b931b93436ba
Bas Boot <b.boot@student.tudelft.nl>
2021-10-06 21:45:39 +0200

    Implemented rotations with integers

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ fn cordic_float(theta: f64) {
     println!("cos({}) = {}", desired_angle, cos_theta);
 }
 
-fn add_values(a: f64, delta_a: f64, sign_a: bool, sign_delta_a: bool) -> (f64, bool) {
+fn add_values(a: u64, delta_a: u64, sign_a: bool, sign_delta_a: bool) -> (u64, bool) {
     // signs not equal, subtract and possible swap values
     if sign_a^sign_delta_a {
         // if second value is larger, swap sign and values
@@ -71,39 +71,37 @@ fn add_values(a: f64, delta_a: f64, sign_a: bool, sign_delta_a: bool) -> (f64, b
 fn cordic_int(theta: f64) {
     // number of iterations
     let n_tan = 10;
-    // precomputed values theta_i, for which tan(theta_i) = 2^{-i} in radians, i = 0..n_tan-1
+    // precomputed values 2^62 * theta_i, for which tan(theta_i) = 2^{-i} in radians, i = 0..n_tan-1
     let precomputed_tan = [0.785398163397448, 0.463647609000806, 0.244978663126864,
-        0.124354994546761, 0.062418809995957, 0.031239833430268,
-        0.015623728620477, 0.007812341060101, 0.003906230131967,
-        0.001953122516479];
+                                    0.124354994546761, 0.062418809995957, 0.031239833430268,
+                                    0.015623728620477, 0.007812341060101, 0.003906230131967,
+                                    0.001953122516479];
     // precomputed scaling factor scaling = cos(theta_0) * ... * cos(theta_{n_tan-1})
     let precomputed_scaling = 0.607253321089875;
 
     let desired_angle = theta;
 
     // initial values
-    let mut x = (1_f64, true);
-    let mut y = (0_f64, true);
+    let mut x = (1_u64 << 62, true);
+    let mut y = (0_u64, true);
     let mut z = desired_angle;  // error
 
     // iterate
     for i in 0..n_tan {
         // take iteration steps
-        assert!(x.0 >= 0.0, "Something went wrong x is negative");
-        assert!(y.0 >= 0.0, "Something went wrong y is negative");
-
-        let delta_x = (power_of_2(-i) * y.0, y.1);
-        let delta_y = (power_of_2(-i) * x.0, x.1);
+        let delta_x = (y.0 >> i, y.1);
+        let delta_y = (x.0 >> i, x.1);
         x = add_values(x.0, delta_x.0, if z > 0_f64 { !delta_x.1 } else { delta_x.1 }, delta_x.1);
         y = add_values(y.0, delta_y.0, if z > 0_f64 { delta_y.1 } else { !delta_y.1 }, delta_y.1);
         z += if z > 0_f64 { -precomputed_tan[i as usize] } else { precomputed_tan[i as usize] };
 
         // println!("i={} x={} y={} z={}",
-        //          i, x.0, y.0, z);
+        //           i, x.0, y.0, z);
     }
 
-    let cos_theta = precomputed_scaling * x.0;
-    let sin_theta = precomputed_scaling * y.0;
+    // convert back to float
+    let cos_theta = precomputed_scaling * (x.0 as f64) / (2_u64.pow(62) as f64);
+    let sin_theta = precomputed_scaling * (y.0 as f64) / (2_u64.pow(62) as f64);
 
     println!("Desired angle {}, approximation error {}", desired_angle, z);
     println!("sin({}) = {}", desired_angle, sin_theta);
@@ -114,9 +112,4 @@ fn cordic_int(theta: f64) {
 fn main() {
     cordic_float(1.0);
     cordic_int(1.0);
-
-    // let test: (f64, bool) = add_values(0.0, 0.0, true, false);
-    //
-    //
-    // print!("Test {} {}", test.1, test.0);
 }
