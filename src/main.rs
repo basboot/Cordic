70f8cf4e1111b309c84dee4ee05dbd90d6fb6b7b
Bas Boot <b.boot@student.tudelft.nl>
2021-10-08 10:02:38 +0200

    Changed angle implementation to int

--- src/main.rs
+++ src/main.rs
@@ -72,22 +72,29 @@ fn add_values(a: u64, delta_a: u64, sign_a: bool, sign_delta_a: bool) -> (u64, b
 }
 
 // approximate sin and cos for theta (radians), using integer operations for rotation
+// theta must be in range 0-2pi
 fn cordic_int(theta: f64) {
     // number of iterations
     let n_tan = 10;
-    // precomputed values 2^62 * theta_i, for which tan(theta_i) = 2^{-i} in radians, i = 0..n_tan-1
-    let precomputed_tan = [0.785398163397448, 0.463647609000806, 0.244978663126864,
-                                    0.124354994546761, 0.062418809995957, 0.031239833430268,
-                                    0.015623728620477, 0.007812341060101, 0.003906230131967,
-                                    0.001953122516479];
+    // precomputed values 2^61 * theta_i, for which tan(theta_i) = 2^{-i} in radians, i = 0..n_tan-1
+    let precomputed_tan: [i64; 10] = [905502432259640320, 534549298976576448, 282441168888798112,
+                                        143371547418228448, 71963988336308048, 36017075762092180,
+                                        18012932708689206, 9007016009513623, 4503576721087964,
+                                        2251796950380271];
+
+
     // precomputed scaling factor scaling = cos(theta_0) * ... * cos(theta_{n_tan-1})
     let precomputed_scaling = 0.607253321089875;
 
-    let desired_angle = theta;
+    // convert float to signed 'fixed point' int sign.3.60
+    let desired_angle = (theta * 2_f64.powf(60_f64)) as i64;
 
     // initial values
+
+    // fixed point 2.62
     let mut x = (1_u64 << 62, true);
     let mut y = (0_u64, true);
+    // 'fixed point' int sign.3.60
     let mut z = desired_angle;  // error
 
     // iterate
@@ -95,9 +102,9 @@ fn cordic_int(theta: f64) {
         // take iteration steps
         let delta_x = (y.0 >> i, y.1);
         let delta_y = (x.0 >> i, x.1);
-        x = add_values(x.0, delta_x.0, if z > 0_f64 { !delta_x.1 } else { delta_x.1 }, delta_x.1);
-        y = add_values(y.0, delta_y.0, if z > 0_f64 { delta_y.1 } else { !delta_y.1 }, delta_y.1);
-        z += if z > 0_f64 { -precomputed_tan[i as usize] } else { precomputed_tan[i as usize] };
+        x = add_values(x.0, delta_x.0, if z > 0 { !delta_x.1 } else { delta_x.1 }, delta_x.1);
+        y = add_values(y.0, delta_y.0, if z > 0 { delta_y.1 } else { !delta_y.1 }, delta_y.1);
+        z += if z > 0 { -precomputed_tan[i as usize] } else { precomputed_tan[i as usize] };
 
         // println!("i={} x={} y={} z={}",
         //           i, x.0, y.0, z);
